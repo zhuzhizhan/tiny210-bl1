e1af6ca383e78b00be5b3219ea1cf20a6b31380b
Zhizhan Zhu <703632667@qq.com>
2018-05-26 21:29:17 +0800

    mmc_relocate: define some parameters
    Signed-off-by: Zhizhan Zhu <703632667@qq.com>

--- mmc_relocate.c
+++ mmc_relocate.c
@@ -1,35 +1,26 @@
-typedef unsigned int (*copy_sd_mmc_to_mem) (unsigned int  channel, unsigned int  start_block, unsigned char block_size, unsigned int  *trg, unsigned int  init);
-
+#include "mmc_relocate.h"
 
 void copy_code_to_dram(void)
 {
 	unsigned long ch;
-	void (*BL2)(void);
-	ch = *(volatile unsigned int *)(0xD0037488);
-
-	// 函数指针
-	copy_sd_mmc_to_mem copy_bl2 = (copy_sd_mmc_to_mem) (*(unsigned int *) (0xD0037F98));
-
 	unsigned int ret;
-	
-	// 通道0
+	void (*BL2)(void);
+
+	ch = *(volatile unsigned int *)(SDRAM_CHANNEL_BASE);
+	copy_sd_mmc_to_mem copy_bl2 = (copy_sd_mmc_to_mem) (*(unsigned int *) (MASKROM_MMC_COPY_BASE));
 	if (ch == 0xEB000000)
 	{
-		// 0:channel 0
-		// 49:源,代码位于扇区49,1 sector = 512 bytes
-		// 32:长度，拷贝32 sector，即16K
-		// 0x23E00000:目的,链接地址0x23E00000
-		ret = copy_bl2(0, 49, 32,(unsigned int *)0x23E00000, 0);
+
+		ret = copy_bl2(0, 49, 32,(unsigned int *)NEXT_FIRMWARE_LDS_BASE, 0);
 	}
-	// 通道2
+
 	else if (ch == 0xEB200000)
 	{
-		ret = copy_bl2(2, 49, 32,(unsigned int *)0x23E00000, 0);
+		ret = copy_bl2(2, 49, 32,(unsigned int *)NEXT_FIRMWARE_LDS_BASE, 0);
 	}
 	else
 		return;
 
-	// 跳转到DRAM
-    BL2 = (void *)0x23E00000;
+    BL2 = (void *)NEXT_FIRMWARE_LDS_BASE;
     (*BL2)();
 }
---
+++ mmc_relocate.h
@@ -0,0 +1,10 @@
+#ifndef __MMC_RELOCATE_H_
+#define __MMC_RELOCATE_H_
+
+typedef unsigned int (*copy_sd_mmc_to_mem) (unsigned int  channel, unsigned int  start_block, unsigned char block_size, unsigned int  *trg, unsigned int  init);
+
+#define SDRAM_CHANNEL_BASE           0xD0037488
+#define MASKROM_MMC_COPY_BASE        0xD0037F98
+#define NEXT_FIRMWARE_LDS_BASE       0x23E00000
+
+#endif
