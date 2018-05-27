821cfc75998973ddae0618e0fb52287949fd6495
Zhizhan Zhu <703632667@qq.com>
2018-05-27 17:48:44 +0800

    mmc relocate: #define NEXT_FIRMWARE_LDS_BASE 0x24000000
    we define the next firmware load address
    to 0x24000000
    
    Signed-off-by: Zhizhan Zhu <703632667@qq.com>

--- mmc_relocate.h
+++ mmc_relocate.h
@@ -5,7 +5,7 @@ typedef unsigned int (*copy_sd_mmc_to_mem) (unsigned int  channel, unsigned int 
 
 #define SDRAM_CHANNEL_BASE           0xD0037488
 #define MASKROM_MMC_COPY_BASE        0xD0037F98
-#define NEXT_FIRMWARE_LDS_BASE       0x23E00000
+#define NEXT_FIRMWARE_LDS_BASE       0x24000000
 #define UBOOT_LOAD_BASE              8193
 #define UBOOT_LOAD_SIZE              2048
 
